1f9c5d6126d35c322b852705e51ae6b896c85b5c
joaofranciscosousa <jfsousacarvalho@gmail.com>
2022-02-23 00:49:51 -0300

    Final

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header.jsx';
+import TinderCards from './TinderCars.jsx';
+import SwipeButtons from './SwipeButtons.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <TinderCards />
+      <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
